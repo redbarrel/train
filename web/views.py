47692eb31eecbb4d107b35f4b75af835ae65372c
kitsawat kaewboonthan <blackrockshooter188@gmail.com>
2022-09-19 15:09:21 +0700

    all

--- web/views.py
+++ web/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+# create your views here.
+
+def Home(request):
+    return render(request, 'web/home.html',)
+
+
+def About(request):
+    return render(request, 'web/about.html',)
+
+
+def Contact(request):
+    return render(request, 'web/contact.html',)
